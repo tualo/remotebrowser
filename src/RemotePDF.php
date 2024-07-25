0dc36f8f42b645966d1878d8caa142fefd0e621d
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-07-25 09:26:03 +0200

    fix remote service

--- src/RemotePDF.php
+++ src/RemotePDF.php
@@ -14,29 +14,31 @@ class RemotePDF{
         $pollingInMilliseconds = 30;
         $timeoutInMilliseconds = 3000;
 
-        $url = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].''.dirname($_SERVER['SCRIPT_NAME']) .''.$db->singleValue('select @sessionid s',[],'s').'/pugreporthtml/'.$tablename.'/'.$template.'/'.$id.'';
-        if (!file_exists(App::get("basePath").'/cache/'.$db->dbname)){
-            mkdir(App::get("basePath").'/cache/'.$db->dbname);
-        }
-        if (!file_exists(App::get("basePath").'/cache/'.$db->dbname.'/ds')){
-            mkdir(App::get("basePath").'/cache/'.$db->dbname.'/ds');
-        }
-        $GLOBALS['pug_cache']=App::get("basePath").'/cache/'.$db->dbname.'/ds';
-        $title = $db->singleValue('select uuid() s',[],'s');
+        if (App::configuration('browsershot','remote_service','')==''){
+            $url = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].''.dirname($_SERVER['SCRIPT_NAME']) .''.$db->singleValue('select @sessionid s',[],'s').'/pugreporthtml/'.$tablename.'/'.$template.'/'.$id.'';
+            if (!file_exists(App::get("basePath").'/cache/'.$db->dbname)){
+                mkdir(App::get("basePath").'/cache/'.$db->dbname);
+            }
+            if (!file_exists(App::get("basePath").'/cache/'.$db->dbname.'/ds')){
+                mkdir(App::get("basePath").'/cache/'.$db->dbname.'/ds');
+            }
+            $GLOBALS['pug_cache']=App::get("basePath").'/cache/'.$db->dbname.'/ds';
+            $title = $db->singleValue('select uuid() s',[],'s');
+                
+            PUGRenderingHelper::exportPUG($db);
+            $html = PUGRenderingHelper::render([$id], $template, [
+                'tablename'=>$tablename,
+            ]);
+            $dom = new DOMDocument();
             
-        PUGRenderingHelper::exportPUG($db);
-        $html = PUGRenderingHelper::render([$id], $template, [
-            'tablename'=>$tablename,
-        ]);
-        $dom = new DOMDocument();
-        
-        if($getTitle){
-            
+            if($getTitle){
+                
 
-            if($dom->loadHTML($html)) {
-                $list = $dom->getElementsByTagName("title");
-                if ($list->length > 0) {
-                    $title = $list->item(0)->textContent;
+                if($dom->loadHTML($html)) {
+                    $list = $dom->getElementsByTagName("title");
+                    if ($list->length > 0) {
+                        $title = $list->item(0)->textContent;
+                    }
                 }
             }
         }
