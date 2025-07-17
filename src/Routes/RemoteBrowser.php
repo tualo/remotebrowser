170e2ad3d95b65864e268ae8b89c948ad9a09df6
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-17 15:10:48 +0200

    Version bump to 1.0.2

--- src/Routes/RemoteBrowser.php
+++ src/Routes/RemoteBrowser.php
@@ -7,31 +7,40 @@ use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 use Tualo\Office\RemoteBrowser\RemotePDF;
-class Route implements IRoute{
 
-    public static function register(){
+class Route implements IRoute
+{
 
-        
-        BasicRoute::add('/remote/pdf/(?P<tablename>[\w\-\_]+)/(?P<template>[\w\-\_]+)/(?P<id>.+)',function($matches){
+    public static function register()
+    {
+
+
+        BasicRoute::add('/remote/pdf/(?P<tablename>[\w\-\_]+)/(?P<template>[\w\-\_]+)/(?P<id>.+)', function ($matches) {
 
             $db = App::get('session')->getDB();
             $sessiondb = App::get('session')->db;
-            try{
-                $res = RemotePDF::get($matches['tablename'],$matches['template'],$matches['id']);
-                if (isset($res['filename'])){
+            try {
+                $res = RemotePDF::get($matches['tablename'], $matches['template'], $matches['id']);
+                if (isset($res['filename'])) {
                     header('Content-type: application/pdf');
+                    header('Content-Disposition: inline; filename="' . $matches['id'] . '.pdf"');
+                    header('Content-Transfer-Encoding: binary');
+                    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+                    header('Access-Control-Allow-Credentials: true');
+                    header('X-Content-Type-Options: nosniff');
+                    header('Cache-Control: must-revalidate');
+                    header('Pragma: public');
+                    header('Expires: 0');
+                    header('Content-Length: ' . filesize($res['filename']));
                     readfile($res['filename']);
                     unlink($res['filename']);
                 }
-                exit();       
-            }catch(\Exception $e){
-                App::result('last_sql', $db->last_sql );
+                exit();
+            } catch (\Exception $e) {
+                App::result('last_sql', $db->last_sql);
                 App::result('msg', $e->getMessage());
             }
             App::contenttype('application/json');
-        },['get','post'],true);
-
-
-
+        }, ['get', 'post'], true);
     }
 }
